85e32b536c26877755343cf3388c5afae8f6a09d
Sandy-HZL <4lessandro.aiello@gmail.com>
2021-02-26 14:31:20 +0100

    added code from session 2

--- public/image-board.js
+++ public/image-board.js
@@ -1,12 +1,30 @@
 new Vue({
     el: "#main",
     data: {
-        imageJSON: null
+        imageJSON: null,
+        title: "",
+        file: null,
     },
     mounted: function (){
         axios.get("/api/images")
             .then((res)=>{
                 this.imageJSON = res.data;
             })
+    },
+    methods: {
+        uploadImage: function(){
+            const uploadData = new FormData();
+
+            uploadData.append("title", this.title)
+            uploadData.append("file", this.file)
+
+            axios.post("/api/upload", uploadData)
+                .then((response)=>{
+                    console.log("upload succesfull", response)
+                })
+        },
+        fileSelected: function (event){
+            this.file = event.target.files[0]
+        }
     }
 })
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require("express")
+const multer = require("multer")
+const uidSafe = ("uid-safe")
+const path = ("path")
 const db = require("./database");
 
 const app= express();
 exports.app = app;
 
-
 app.use(express.static("public"))
+
+const diskStorage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        const destinationDirectory = __dirname + "/uploads";
+        callback(null, destinationDirectory);
+    },
+    filename: (request, file, callback) => {
+        uidSafe(24).then(uid => {
+            const originalExtension = path.extname(file.originalname)
+            const filename = uid + originalExtension;
+            callback(null, filename)
+        })
+    }
+})
+const uploader = multer({
+    limits: {
+        fileSize: 5242880,
+    },
+    storage: diskStorage
+})
 
 app.get("/api/images", (req,res) => {
     db.getImages()
@@ -16,6 +38,9 @@ app.get("/api/images", (req,res) => {
 
         })
         .catch(err=>console.log(err))
+})
+
+app.post("/api/upload",uploader.single("file"),(req,res) =>{
 })
 
 app.listen(8080)
