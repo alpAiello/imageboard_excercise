e3cb8ebbb24b20e128871528749174093d626c9c
Sandy-HZL <4lessandro.aiello@gmail.com>
2021-02-28 22:13:25 +0100

    added amazon s3

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const uidSafe = require("uid-safe");
 const path = require("path");
 const db = require("./database");
+const s3 = require("./s3");
 
 const app = express();
 exports.app = app;
@@ -41,11 +42,18 @@ app.get("/api/images", (req, res) => {
 });
 
 app.post("/api/upload", uploader.single("file"), (req, res) => {
-    console.log(req.file);
-    db.addImage(req.body.title, req.body.description, req.file.filename)
-        .then(data => {
-            console.log(data.rows[0]);
-            res.send(data.rows[0]);
+    console.log("Super! Upload ist im Gange..");
+    console.log("request.body", req.body);
+    console.log("request.file", req.file);
+    s3.uploadFile(req.file)
+        .then((result) => {
+            console.log('S3 result', result);
+            const fileURL = s3.getS3URL(req.file.filename);
+            db.addImage(req.body.title, req.body.description,fileURL)
+                .then(data => {
+                    console.log(data.rows[0]);
+                    res.send(data.rows[0]);
+                });
         });
 });
 
